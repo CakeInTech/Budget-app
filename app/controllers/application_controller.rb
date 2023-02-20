2d6b913cea85680b8dfe353728ae81d0175a1aac
MohamedCK <abdulhakimm280@gmail.com>
2023-02-20 20:34:18 +0200

    - Add authentication and Devise sanitizer for user parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    groups_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :current_password])
+  end
 end
