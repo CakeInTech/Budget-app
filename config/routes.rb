a47f175752f7d5112fa80b43849d1ef25816a559
MohamedCK <abdulhakimm280@gmail.com>
2023-02-20 12:42:14 +0200

    - Setup the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'users#index'
+  
+  resources :users do
+   resources :categories, controller: 'groups', as: 'groups' do
+      resources :transactions, controller: 'expenses', as: 'expenses'
+    end
+  end
 end
