72f798fe9c3907435b7235653bd76aa39d09c5d3
MohamedCK <abdulhakimm280@gmail.com>
2023-02-20 12:59:29 +0200

    - Setup validations for expense

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,4 +2,7 @@ class Expense < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :group_expenses, dependent: :destroy
   has_many :groups, through: :group_budgets, dependent: :destroy
+
+  validates :name, presence: true, length: { minimum: 1, maximum: 20 }, uniqueness: { scope: :author, message: 'You already have a transaction with this name'}
+  validates :amount, presence: true, numericality: { greater_that_or_equal_to: 0 }
 end
