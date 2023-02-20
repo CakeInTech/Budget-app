72b26d3a22079d8de5b442136f55faa3a4a2ca25
MohamedCK <abdulhakimm280@gmail.com>
2023-02-20 21:03:21 +0200

    - Fix github linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
+
+
   has_many :groups, foreign_key: :author_id, dependent: :destroy
   has_many :expenses, foreign_key: :author_id, dependent: :destroy
 
