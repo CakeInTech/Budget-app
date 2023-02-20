cafe6eda7418ea7a54e77ffde2806e6bc2716df5
MohamedCK <abdulhakimm280@gmail.com>
2023-02-20 18:59:49 +0200

    - Set the default url options for the device

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,9 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener
+  
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
