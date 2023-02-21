d844b667e9343f62414a51e2cd20b78833d5a984
MohamedCK <abdulhakimm280@gmail.com>
2023-02-21 15:49:59 +0200

    - Refactor controller actions for Expense model

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,69 +1,70 @@
 class ExpensesController < ApplicationController
-  before_action :set_expense, only: %i[show edit update destroy]
+  before_action :set_expense, only: %i[edit update destroy]
+  before_action :set_group, only: %i[index new edit create update destroy]
+  before_action :set_author, only: %i[index edit create update destroy]
 
   # GET /expenses or /expenses.json
+  # Gets all of the expenses for a specific group, ordered by created_at
   def index
-    @expenses = Expense.all
+    @expenses = @group.expenses.order(created_at: :desc)
   end
 
-  # GET /expenses/1 or /expenses/1.json
-  def show; end
-
-  # GET /expenses/new
+  # GET group/expenses/new
+  # Render the form to create a new expense
   def new
     @expense = Expense.new
   end
 
-  # GET /expenses/1/edit
+  # GET group/expenses/1/edit
+  # Get an existing expense record to render the edit form
   def edit; end
 
-  # POST /expenses or /expenses.json
+  # POST group/expenses
+  # Create a new expense
   def create
-    @expense = Expense.new(expense_params)
-
-    respond_to do |format|
-      if @expense.save
-        format.html { redirect_to expense_url(@expense), notice: 'Expense was successfully created.' }
-        format.json { render :show, status: :created, location: @expense }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @expense.errors, status: :unprocessable_entity }
-      end
+    @expense = Expense.new(author: @author, **expense_params)
+    if @expense.save
+      @group_expense = GroupExpense.create(group: @group, expense: @expense)
+      redirect_to group_expenses_url(@group), notice: 'Expense was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /expenses/1 or /expenses/1.json
+  # PATCH/PUT group/expenses/1
+  # Updates an existing expense in the database
   def update
-    respond_to do |format|
-      if @expense.update(expense_params)
-        format.html { redirect_to expense_url(@expense), notice: 'Expense was successfully updated.' }
-        format.json { render :show, status: :ok, location: @expense }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @expense.errors, status: :unprocessable_entity }
-      end
+    if @expense.update(expense_params)
+      redirect_to group_expenses_url, notice: 'Expense was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /expenses/1 or /expenses/1.json
+  # DELETE group/expenses/1
+  # Deletes an existing expense from the database
   def destroy
     @expense.destroy
-
-    respond_to do |format|
-      format.html { redirect_to expenses_url, notice: 'Expense was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to group_expenses_url(@group), notice: 'Expense was successfully destroyed.'
   end
 
   private
 
   # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @author = current_user
+  end
+
+  def set_group
+    @group = set_user.groups.find(params[:group_id])
+  end
+
   def set_expense
-    @expense = Expense.find(params[:id])
+    @expense = set_user.expenses.find(params[:id])
   end
 
   # Only allow a list of trusted parameters through.
   def expense_params
-    params.require(:expense).permit(:name, :amount, :author_id)
+    params.require(:expense).permit(:name, :amount)
   end
 end
