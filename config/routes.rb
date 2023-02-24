9e0853e64af0936c3826f86bd7f64072cf51e154
MohamedCK <abdulhakimm280@gmail.com>
2023-02-24 16:03:42 +0200

    - Update the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
   root 'splash_screen#splash'
+  get '/splash', to: 'splash_screen#splash'
 
+  resources :users
+  resources :groups, :path => "categories" do 
+    resources :expenses, :path => "transactions"
+  end
+
+  
   devise_for :users
 
   devise_scope :user do
     # root to: 'devise/sessions#new'
   end
   
-  resources :users do
-   resources :categories, controller: 'groups', as: 'groups' do
-      resources :transactions, controller: 'expenses', as: 'expenses'
-    end
-  end
 end
