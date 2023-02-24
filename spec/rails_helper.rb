92fa6de6277c8b68d6d7894a289c48d10f8968c1
MohamedCK <abdulhakimm280@gmail.com>
2023-02-24 17:33:37 +0200

    - Configure the database cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,6 +33,24 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{Rails.root}/spec/fixtures"
 
+  config.before(:suite) do 
+    DatabaseCleaner.clean_with(:truncation)
+  end
+  config.before(:each) do 
+    DatabaseCleaner.stategy = :transaction
+  end
+  config.before(:each, js: true) do 
+    DatabaseCleaner.stategy = :truncation
+  end
+  # This block must be here, do not combine with the other `before(:each)` block.
+  # This makes it so Capybara can see the database.
+  config.before(:each) do 
+    DatabaseCleaner.start
+  end
+  config.after(:each) do 
+    DatabaseCleaner.clean
+  end
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
